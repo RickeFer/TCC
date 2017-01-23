7d534c4a088f712c8fe22637c315bdb04e8779b4
rickefer <henrique.fer@hotmail.com>
2017-01-23 21:28:37 -0200

    bug fix 2

--- TG/settings.py
+++ TG/settings.py
@@ -161,7 +161,7 @@ if ONLINE:
     import dj_database_url
 
     DATABASES = {
-        'default': dj_database_url.config(dafault='postgres://localhost')
+        'default': dj_database_url.config(default='postgres://localhost')
     }
 
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
