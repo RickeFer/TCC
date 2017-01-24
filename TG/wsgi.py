9a26224aebdc94cde7f1b00cf740d528a28d5576
rickefer <henrique.fer@hotmail.com>
2017-01-23 22:04:28 -0200

    wsgi fix

--- TG/wsgi.py
+++ TG/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TG.settings")
 #application = get_wsgi_application()
 application = Cling(get_wsgi_application())
