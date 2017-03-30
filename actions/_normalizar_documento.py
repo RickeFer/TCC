657b22c54d1d94a4770a2854e63fedd32a81efb1
henrique <henrique.fer@hotmail.com>
2017-03-30 18:44:16 -0300

    indo pra 3fn

--- actions/_normalizar_documento.py
+++ actions/_normalizar_documento.py
@@ -100,21 +100,36 @@ def run_normalizar_documento(request, documento_id):
             tabelas_renomear = documento.tabela_set.filter(renomear=True)
 
             array_tabelas = []
-            for tabela in tabelas_renomear:
-                temp_rel = tabela.campo_tabela_set.all()
-
-                campos = []
-                for rel in temp_rel:
-                    temp_campo = Campo.objects.get(id=rel.campo.id)
-                    campos.append(temp_campo)
-
-                aux = {'tabela': tabela, 'campos': campos}
-                array_tabelas.append(aux)
-
             if len(tabelas_renomear):
+                for tabela in tabelas_renomear:
+                    temp_rel = tabela.campo_tabela_set.all()
+
+                    campos = []
+                    for rel in temp_rel:
+                        temp_campo = Campo.objects.get(id=rel.campo.id)
+                        campos.append(temp_campo)
+
+                    aux = {'tabela': tabela, 'campos': campos}
+                    array_tabelas.append(aux)
+
                 context = {'renomear_tabelas': True, 'tabelas': array_tabelas, 'documento': documento}
             else:
-                context = {'documento': documento, 'fn': fn}
+                tabelas = documento.tabela_set.filter(tabela_tipo=1)
+
+                for tabela in tabelas:
+                    temp_rel = tabela.campo_tabela_set.filter(tipo_campo='Normal')
+
+                    campos = []
+                    if len(temp_rel):
+                        for rel in temp_rel:
+                            temp_campo = Campo.objects.get(id=rel.campo.id)
+                            campos.append(temp_campo)
+
+                        aux = {'tabela': tabela, 'campos': campos}
+                        array_tabelas.append(aux)
+
+                context = {'documento': documento, 'fn': fn, 'arrayTabelas': array_tabelas}
+
 
         return context
     else:
